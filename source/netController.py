42e59dac346765590cdc14c9c40014c1a5efcfc2
giacomo-ascari <giacomolego@gmail.com>
2022-02-17 15:46:14 +0100

    update: implement morphing and collapsing routers

--- source/netController.py
+++ source/netController.py
@@ -57,43 +57,25 @@ def addVirtualSwitch(net,name,index):
 class NetController():
     def __init__(self, count):
         info("[NC] instance init\n")
-        self.net = Containernet(controller=Controller, link=TCLink, switch=OVSKernelSwitch, topo=EmptyTopo(), build=False)
+        self.net = Mininet(controller=Controller, link=TCLink, switch=OVSKernelSwitch, topo=EmptyTopo(), build=False)
         self.count = count
         c0 = self.net.addController("c0")
         for i in range(0, count):
             host = addHostR(self.net, "h" + str(i+1), i+1)
             id = "r" + str(i+1)
             router = addRouter(self.net, id,i+1)
-            
-        
         self.topoController = TopoController()
-        self.topoController.morph(self.net, "string",count)
-        self.index = 0
         self.slic = SlicController()
-        #self.net.build()
-        #c0.start()
-        #self.topoController.define_interfaces(self.net,4)
     
     def start(self):
         info("[NC] start\n")
+        self.topoController.morph(self.net, "ring", self.count)
         self.net.build()
         self.net.start()
-        self.slic.morph(self.net, "string", "string", self.count)
-        addVirtualHost(self.net,"r5",5)
-        self.net["r5"].cmd("ifconfig r5-eth5 0")
-        self.net["r4"].cmd("ifconfig r4-eth1 0")
-
-        self.net.addLink(self.net.get("r4"), self.net.get("r5"),
-                    intfName1="r4-eth1",
-                    params1={'ip':'10.0.45.1/24'},
-                    intfName2="r5-eth2",
-                    params2={'ip':'10.0.45.2/24'}
-        )
-        #
-        # self.net.addLink(self.net["r5"],self.net["r1"])
-        #self.net["r5"].cmd
-
-        self.test()
+        self.slic.morph(self.net, "ring", "string", self.count)
+        self.slic.collapseRouter(self.net, 2)
+        self.slic.morph(self.net, "ring", "ring", self.count)
+        #self.test()
         #info("\n\n\n")
         #self.slic.morph2(self.net, "string", "string", self.count)
 
@@ -104,9 +86,6 @@ class NetController():
         self.index += 1
         self.net.build()
         self.net.start()'''
-
-    def collapseSwitch(self,index):
-        pass
 
     def deployVRouter(self):
         pass
--- source/slicController.py
+++ source/slicController.py
@@ -32,7 +32,7 @@ class SlicController():
                 if l.find("via") > 0:
                     net[name].cmd("ip route del " + l)
         
-        if physicalTopo == "string" and virtualTopo == "string":
+        if (physicalTopo == "string" or physicalTopo == "ring") and virtualTopo == "string":
             for i in range(1, count+1):
                 router = net.get("r" + str(i))
                 addRoute(router, "10.0."+str(i)+".1/32", "10.0."+str(i)+".254", "r"+str(i)+"-eth0")
@@ -54,9 +54,17 @@ class SlicController():
         elif physicalTopo == "string" and virtualTopo == "ring":
             #morph, eventuale collapse di un router
             pass
+
         elif physicalTopo == "ring" and virtualTopo == "ring":
-            #impostare route "verso destra"
-            pass
-        elif physicalTopo == "ring" and virtualTopo == "string":
-            #morphing di qualche tipo
-            pass
+            for i in range(1, count+1):
+                router = net.get("r" + str(i))
+                addRoute(router, "10.0."+str(i)+".1/32", "10.0."+str(i)+".254", "r"+str(i)+"-eth0")
+                remote = "10.0.0.0/16"
+                nexthop = "10.0." + str(i) + str((i) % count + 1) + ".2"
+                intf = "r" + str(i) + "-eth1"
+                addRoute(router, remote, nexthop, intf)
+    
+    def collapseRouter(self, net, index):
+        name = "r" + str(index)
+        net[name].cmd("ip route del 10.0." + str(index) + ".0/24")
+        net[name].cmd("ip route del 10.0." + str(index) + ".1")
