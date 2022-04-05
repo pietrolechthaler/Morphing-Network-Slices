a8a86c92bccd03f29708299ce1631c14d22ecb0e
Pietro Lechthaler <pietrolechthaler@gmail.com>
2022-04-05 13:24:24 +0200

    Update README

--- testing/hub.py
+++ testing/hub.py
@@ -1,40 +1,60 @@
-#!/usr/bin/env python
-
-from mininet.cli import CLI
-from mininet.link import Link,TCLink,Intf
 from mininet.net import Mininet
-from mininet.node import RemoteController
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from _thread import start_new_thread
+import os, stat
+import json
+import time
+import csv
+import requests
+import sys
 
- 
 
-if '__main__' == __name__:
+def four_switches_network():
+    http_link_config = dict(bw=1)
+    net = Mininet(topo=None,
+        switch=OVSKernelSwitch,
+        build=False,
+        autoSetMacs=True,
+        autoStaticArp=True,
+        link=TCLink)
+                   
 
-  net = Mininet(link=TCLink)
-  h1 = net.addHost('h1', mac='00:00:00:00:01:00')
-  h2 = net.addHost('h2', mac='00:00:00:00:02:00')
-  h3 = net.addHost('h3', mac='00:00:00:00:03:00')
-  h4 = net.addHost('h4', mac='00:00:00:00:04:00')
+    info('*** Add switches\n')
+    h1 = net.addHost('h1', mac='00:00:00:00:00:01')
+    h2 = net.addHost('h2', mac='00:00:00:00:00:02')
+    h4 = net.addHost('h4', mac='00:00:00:00:00:04')
 
-  s1 = net.addSwitch("s1")
-  s2 = net.addSwitch("s2")
-  s3 = net.addSwitch("s3")
+    s1 = net.addSwitch("s1")
+    s2 = net.addSwitch("s2")
 
-  net.addLink("h1", "s1")
-  net.addLink("h2", "s2")
-  net.addLink("h3", "s3")
-  net.addLink("s1", "h4")
-  net.addLink("s2", "h4")
-  net.addLink("s3", "h4")
+    net.addLink("h1", "s1")
+    net.addLink("h2", "s2")
+    net.addLink("s1", "h4")
+    net.addLink("s2", "h4")
 
-  net.build()
-  h4.cmd("ifconfig h4-eth0 0")
-  h4.cmd("ifconfig h4-eth1 0")
-  h4.cmd("ifconfig h4-eth2 0")
-  h4.cmd("brctl addbr br0")
-  h4.cmd("brctl addif br0 h4-eth0")
-  h4.cmd("brctl addif br0 h4-eth1")
-  #h4.cmd("brctl setageing br0 0")
-  h4.cmd("brctl addif br0 h4-eth2")
-  h4.cmd("ifconfig br0 up")
-  CLI(net)
-  net.stop()
+    info('*** Starting network\n')
+    controller = RemoteController("c1", ip="127.0.0.1", port=6633)
+    net.addController(controller)
+    net.build()
+    net.start()
+    net.build()
+    h4.cmd("ifconfig h4-eth0 0")
+    h4.cmd("ifconfig h4-eth1 0")
+    h4.cmd("brctl addbr br0")
+    h4.cmd("brctl addif br0 h4-eth0")
+    h4.cmd("brctl addif br0 h4-eth1")
+    #h4.cmd("brctl setageing br0
+    h4.cmd("brctl addif br0 h4-eth2")
+    h4.cmd("ifconfig br0 up")
+    CLI(net)
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+four_switches_network()
